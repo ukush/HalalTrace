d343406cd9d7984a2738047264782e23894554c1
ukush <ukushi01@gmail.com>
2024-03-03 07:30:54 +0000

    nft metadata

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -10,19 +10,39 @@ contract NFT {
      * Start with simple metadata
      * Initialised when NFT is first minted
      * ----------------------
-     * ID 
-     * Breed
-     * Herd Number
-     * DOB
-     * Scheduled slaughter date
+     * ID - uint256
+     * Breed - string
+     * Herd Number - uint64
+     * DOB - ?
+     * Scheduled slaughter date - ?
      * 
      * Initisalised after
      * ------------------
      * Slaughterhouse data:
-     * date/time of arrival
-     * slaughter method
-     * slaughter instrument
-     * pre-stunned
+     * ----
+     * date/time of arrival - ?
+     * slaughter method - string
+     * slaughter instrument - string
+     * pre-stunned - bool
+     * 
+     * product data
+     * ----
+     * cuts' ids - list of uint256
+     * 
+     * Processor data
+     * -----
+     * production data - ?
+     * best before date - ?
+     * batch/lot id - uint256
+     * product id - uint256
+     * 
+     * Distributor data
+     * ----
+     * date of arrival
+     * 
+     * Retailer Data
+     * ----
+     * date of arrivals
      * 
      * 
      */
