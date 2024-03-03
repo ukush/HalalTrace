0263894c90a764f85e28788855eeb3ad1fb78aab
ukush <ukushi01@gmail.com>
2024-03-03 07:19:24 +0000

    nft contract create file

--- contracts/Lock.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.24;
-
-import "hardhat/console.sol";
-
-contract NFT {
-    
-}
---
+++ contracts/NFT.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import "hardhat/console.sol";
+
+contract NFT {
+    /**
+     * NFT Metadata
+     * 
+     * Start with simple metadata
+     * Initialised when NFT is first minted
+     * ----------------------
+     * ID 
+     * Breed
+     * Herd Number
+     * DOB
+     * Scheduled slaughter date
+     * 
+     * Initisalised after
+     * ------------------
+     * Slaughterhouse data:
+     * date/time of arrival
+     * slaughter method
+     * slaughter instrument
+     * pre-stunned
+     * 
+     * 
+     */
+
+}
