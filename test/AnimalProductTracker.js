13eb8808aa57f56cb0e24da569738df7ea87210c
ukush <ukushi01@gmail.com>
2024-03-07 14:32:38 +0000

    remove test case

--- test/AnimalProductTracker.js
+++ test/AnimalProductTracker.js
@@ -10,17 +10,17 @@ const { expect } = require("chai");
 describe("Token contract", function () {
   it("Deployment should assign the admin role to the owner", async function () {
     // deploy a AnimalProductTracker contract
-    const trackerContract = await ethers.deployContract("AnimalTrackerContract", [adminAddress], {
-      value: 0x00
-    });
+    // const trackerContract = await ethers.deployContract("AnimalTrackerContract", [adminAddress], {
+    //   value: 0x00
+    // });
 
-    //query admin priveledged accounts
-    const adminCount = await trackerContract.getRoleMemberCount(DEFAULT_ADMIN_ROLE);
+    // //query admin priveledged accounts
+    // const adminCount = await trackerContract.getRoleMemberCount(DEFAULT_ADMIN_ROLE);
 
-    const members = [];
-    for (let i = 0; i < adminCount; ++i) {
-        members.push(await trackerContract.getRoleMember(DEFAULT_ADMIN_ROLE, i));
-      }
+    // const members = [];
+    // for (let i = 0; i < adminCount; ++i) {
+    //     members.push(await trackerContract.getRoleMember(DEFAULT_ADMIN_ROLE, i));
+    //   }
 
   });
 
