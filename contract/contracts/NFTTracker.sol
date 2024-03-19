cd91b882cd16399144ac266f12a649fd04094f25
ukush <ukushi01@gmail.comm>
2024-03-19 16:02:25 +0000

    new tracker smart contract

--- contract/contracts/NFTTracker.sol
+++ contract/contracts/NFTTracker.sol
@@ -6,80 +6,69 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract AnimalProductTracker is ERC721, ERC721URIStorage, AccessControl {
+contract AnimalProductTracker is ERC721("HalalTrace", "HTrace") { 
+
+    /**
+     * @dev defines an event which occurs at the major control points of the supply chain
+     */
+    struct Event {
+        address participant;
+        string dataURI;
+        uint256 timestamp;
+    }
 
     /**
     * @dev defines the basic metadata of each animal NFT.
-    * Additional attributes will be stored off-chain on IPFS
     */
-
-    struct AnimalNFT {
-        uint256 AnimalID;
+    struct NFT {
+        uint256 id;
         string animalType;
         string breed;
         uint256 herdNumber;
-        uint256 supplyChainStage;
+        Event[] trace;
+        uint256[] intervals;
     }
 
-    // Container to hold our NFTs
-    AnimalNFT[] public animalNFTCollection;
+    /**
+    * @dev maps ids to products
+    */
+    mapping (uint256 => NFT) private id2NFT;
 
-    // mappings will go here
-    mapping(address => animalNFTCollection) public ownerToNFTCollection;
-    mapping(uint256 => uint256) public tokenIDToAnimalNFT;
+    /**
+     * @dev function to mint animal nft
+     * @param animalId the unique id of the animal on the farm
+     * @param _type the type of animal
+     * @param _breed the breed of the animal
+     * @param _herdNum the herd number of the animal
+     */
+    function createAnimalNFT(uint256 animalId, string memory _type, string memory _breed, uint256 _herdNum) public returns(uint256) {
 
+        NFT storage nft = id2NFT[animalId];
+        nft.id = animalId;
+        nft.animalType = _type;
+        nft.breed = _breed;
+        nft.herdNumber = _herdNum;
+        nft.intervals.push(block.timestamp);
 
-    bytes32 public constant FARMER_ROLE = keccak256("MINTER_ROLE");
-    uint256 private _nextTokenId;
-
-    constructor()
-        ERC721("AnimalProductTracker", "HalalTrace")
-    {
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _safeMint(msg.sender, animalId);
+        return animalId;
     }
 
-    function mintNFT(
-    address to,
-    string memory animalType,
-    string memory breed,
-    uint256 herdNumber,
-    string memory uri
-    ) public onlyRole(FARMER_ROLE) {
-        
-        uint256 tokenId = _nextTokenId++;
-        
-        animalNFTCollection.push(
-            AnimalNFT {
-                animalID : tokenId,
-                animalType : animalType,
-                breed : breed,
-                herdNumber : herdNumber,
-                supplyChainStage : uint256("Created")
-            }
-        );
-        tokenIDToAnimalNFT[tokenId] = tokenId;
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
+    /**
+     * @dev function to update the event metadata
+     * @param animalId the unique id of the animal
+     * @param dataURI the new uri pointing to IPFS file
+     */
+    function update(uint256 animalId, string memory dataURI) public {
+        NFT storage nft = id2NFT[animalId];
+        nft.trace.push(Event(msg.sender, dataURI, block.timestamp));
     }
 
-    // The following functions are overrides required by Solidity.
-
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
-    }
-
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721URIStorage, AccessControl)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
+    /**
+    * @dev function to return the details for a given animal product
+    */
+    function getAnimalDetails(uint256 animalId) public view returns (NFT memory) {
+        return id2NFT[animalId];
     }
 }
 
