1cd72c958138709828329f79ff0f7a5f387f28d9
ukush <ukushi01@gmail.comm>
2024-04-02 15:21:07 +0100

    ignition deploy

---
+++ contract/ignition/modules/NFTTracker.js
@@ -0,0 +1,9 @@
+const { buildModule } = require('@nomicfoundation/hardhat-ignition/modules');
+
+
+module.exports = buildModule('NFTTracker', (m) => {
+
+    const tokenContract = m.contract("ProductTracker");
+
+    return {tokenContract};
+});
