cde0f47764bf88701630efec546136fa0dc3c6af
ukush <ukushi01@gmail.comm>
2024-04-02 15:21:24 +0100

    updates

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -3,27 +3,28 @@ require("hardhat-contract-sizer");
 require("hardhat-gas-reporter");
 require("dotenv").config();
 require("@nomicfoundation/hardhat-ethers");
+require("@nomicfoundation/hardhat-ignition-ethers");
 
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
-  contractSizer: {
-    alphaSort: true,
-    disambiguatePaths: false,
-    runOnCompile: false,
-    strict: true,
-  },
-  gasReporter: {
-    enabled: false
-  },
+  solidity: "0.8.20",
   networks: {
     hardhat: {},
     mumbai: {
-      url: process.env.MUMBAI_API_URL,
+    url: process.env.MUMBAI_URL,
       accounts: [`0x${process.env.PRIVATE_KEY}`],
     },
   },
+  contractSizer: {
+    alphaSort: true,
+    disambiguatePaths: false,
+    runOnCompile: process.env.CONTRACT_SIZER,
+    strict: true,
+  },
+  gasReporter: {
+    enabled: process.env.GAS_REPORTER
+  }
 };
 
 
