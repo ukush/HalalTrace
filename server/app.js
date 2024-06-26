0787d024ab63bfb53caca85a9637e997c2216ad2
ukush <ukushi01@gmail.comm>
2024-06-26 18:01:03 +0100

    remove unnecessary files

--- server/app.js
+++ server/app.js
@@ -8,14 +8,13 @@ var cors = require('cors');
 
 // import routes
 var indexRouter = require('./routes/index');
-// var userRouter = require('./routes/users')
 var apiRouter = require('./routes/api');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -27,7 +26,6 @@ app.use(cors());
 
 // use routes
 app.use('/', indexRouter)
-// app.use('/users', userRouter)
 app.use('/api', apiRouter)
 
 
@@ -43,8 +41,10 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status || 500).json({
+    message: err.message,
+    error: err
+  });
 });
 
 module.exports = app;
