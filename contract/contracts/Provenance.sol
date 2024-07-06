51ca63cfe8081a05cfba19d6ba5e8be927632e91
ukush <ukushi01@gmail.com>
2024-07-06 15:08:17 +0100

    changes

--- contract/contracts/Provenance.sol
+++ contract/contracts/Provenance.sol
@@ -10,11 +10,6 @@ pragma solidity >=0.5.0 <0.9.0;
  * barcode
  */
 
-/**
- * To reduce gas... what needs to be in persistent storage??
- * Both mappings, products and producers
- * Supply chain events associated with those products
- */
 contract Provenance { 
 
     address public owner;
@@ -32,7 +27,7 @@ contract Provenance {
      * @dev Struct to store essential product info on-chain.
      */
     struct Product {
-        string name;
+        bytes32 name;
         address producer;
         uint256 created;
         ScEvent[] ScEvents;
@@ -43,13 +38,15 @@ contract Provenance {
      */
     // do we need this on chain? or could we do with off-chain storage for this?
     struct Producer {
-        string name;
-        string country;
-        string zip;
+        bytes32 name;
+        bytes32 country;
+        bytes32 zip; // max of 6 characters? Use fixed length bytes
         bool certified;
         // could have something here like a hash of their certificate for ESG credentials
         // e.g. certificate of carbon emissions
     }
+
+    mapping(uint256 => uint256[]) batches;
 
     // maps product id to product details
     mapping(uint256 => Product) products;
@@ -67,84 +64,89 @@ contract Provenance {
      * @param _zip the zip/postcode of the producer's operation location
      * 
      */
-    function addProducer(string memory _name, string memory _country, string memory _zip) public {
+    function addProducer(bytes32 _name, bytes32 _country, bytes32 _zip) public {
         // check that the company is not aready registered
-        bytes memory nameStr = bytes(producers[msg.sender].name);
-        require(nameStr.length == 0 , "Company wallet already registered");
-
+        require(producers[msg.sender].name.length == 0 , "Company wallet already registered");
         // add producer
         producers[msg.sender] = Producer(_name, _country, _zip, false);
     }
 
     /**
      * Function for admin to certify a producer
-     * @param producer the public address of the producer
+     * @param _producer the public address of the producer
      */
-    function certifyProducer(address producer) public {
+    function certifyProducer(address _producer) public {
         // The producer should be automatically certified once their docs have been checked
         // For now let's give the authority to the owner of the contract
         require(msg.sender == owner, "Only the contract owner can certify producers");
 
-        if (producers[producer].certified == false) {
+        if (producers[_producer].certified == false) {
             // certify
-            producers[producer].certified = true;
+            producers[_producer].certified = true;
         }
     }
 
     /**
      * Function allowing a certified producer to add a new product to the chain
-     * @param productId the product identifier (gathered from the physical barcode)
-     * @param name the name of the product
+     * @param _productId the product identifier (gathered from the physical barcode)
+     * @param _name the name of the product
      */
-    function addProduct(uint256 productId, string memory name) public returns(uint256) {
+    function addProduct(uint256 _productId, bytes32 _name) public returns(uint256) {
         // check that the producer is certifed
         require(producers[msg.sender].certified == true, "Only certified producers can add new products");
         // check that the product doesn't already exist
-        bytes memory nameStr = bytes(products[productId].name);
-        require(nameStr.length == 0 , "That product already exists");
+        require(products[_productId].name.length == 0 , "That product already exists");
 
         // add product to mapping
-        ScEvent[] memory events;
-        Product memory product = Product(name, msg.sender, block.timestamp, events);
-        products[productId] = product;
-
+        Product storage product = products[_productId];
+        product.name = _name;
+        product.producer = msg.sender;
+        product.created = block.timestamp;
+    
         // return the timestamp of creation
         return product.created;
     }
 
+    function addBatchProducts(uint256 _batchNo, uint256[] memory _productIds) public {
+        // ensure the batch doesn't already exist on-chain
+        require(batches[_batchNo].length == 0, "A batch of products already exists with that batch id");
+        for (uint i = 0; i < _productIds.length; i++) {
+            //addProduct(_productIds[i]);
+        }
+    }
+
+
     /**
      * Function to link supply chain activity data to a product
-     * @param productId the product identifier (gathered from the physical barcode)
-     * @param eventURI the CID (content hash) of the IPFS file containing event data
+     * @param _productId the product identifier (gathered from the physical barcode)
+     * @param _eventURI the CID (content hash) of the IPFS file containing event data
      */
-    function addProductEvent(uint256 productId, bytes32 eventURI) public {
+    function addProductEvent(uint256 _productId, bytes32 _eventURI) public {
         // check that the producer is certifed
         require(producers[msg.sender].certified == true, "Only certified producers can add new products");
         // check that the product doesn't already exist
-        bytes memory nameStr = bytes(products[productId].name);
-        require(nameStr.length == 0 , "That product already exists");
+        require(products[_productId].name.length == 0 , "That product already exists");
 
-        Product storage product = products[productId];
-        product.ScEvents.push(ScEvent(eventURI, block.timestamp));
+        Product storage product = products[_productId];
+        product.ScEvents.push(ScEvent(_eventURI, block.timestamp));
     }
 
     /**
      * Function that returns the product details given a product Id
-     * @param productId the product identifier (gathered from the physical barcode)
-     * @return name the product name
-     * @return address the public address of the producer of this product
-     * @return created the timestamp of the creation of the product on chain
-     * @return ScEvent[] array of the events associated with the product
+     * @param _productId the product identifier (gathered from the physical barcode)
+     * @return _name the product name
+     * @return _address the public address of the producer of this product
+     * @return _created the timestamp of the creation of the product on chain
+     * @return _scEvent array of the events associated with the product
      */
-    function findProduct(uint256 productId) public view returns(string memory, address, uint, ScEvent[] memory) {
-        bytes memory nameStr = bytes(products[productId].name);
-        require(nameStr.length != 0 , "That product already exists");
+    function findProduct(uint256 _productId) public view returns(bytes32, address, uint, ScEvent[] memory) {
+        require(products[_productId].name.length != 0 , "That product already exists");
 
         return (
-            products[productId].name,
-            products[productId].producer,
-            products[productId].created,
-            products[productId].ScEvents
+            products[_productId].name,
+            products[_productId].producer,
+            products[_productId].created,
+            products[_productId].ScEvents
         );
     }
 }
